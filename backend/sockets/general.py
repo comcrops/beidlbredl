f8e55e6566c338fce8794731e547ba8caf3cdc46
s-prechtl <stefan@tague.at>
2026-05-23 20:16:09 +0200

    feat: implement /general Socket.IO namespace for carousel and app management

--- backend/sockets/general.py
+++ backend/sockets/general.py
@@ -1,5 +1,42 @@
-from flask_socketio import Namespace
+from flask_socketio import Namespace, emit
+from state import kiosk_state
 
 
 class GeneralNamespace(Namespace):
-    pass
+    def on_connect(self):
+        emit('state', kiosk_state.to_dict())
+
+    def on_open_app(self, data):
+        app_id = data.get('app_id')
+        if app_id and app_id not in kiosk_state.open_app_ids:
+            kiosk_state.open_app_ids.append(app_id)
+        kiosk_state.active_app_id = app_id
+        self._broadcast_state()
+
+    def on_close_app(self, data):
+        app_id = data.get('app_id')
+        if app_id in kiosk_state.open_app_ids:
+            kiosk_state.open_app_ids.remove(app_id)
+        if kiosk_state.active_app_id == app_id:
+            kiosk_state.active_app_id = kiosk_state.open_app_ids[-1] if kiosk_state.open_app_ids else None
+        self._broadcast_state()
+
+    def on_carousel_next(self):
+        self._carousel_step(1)
+
+    def on_carousel_prev(self):
+        self._carousel_step(-1)
+
+    def _carousel_step(self, direction: int):
+        if not kiosk_state.open_app_ids:
+            return
+        ids = kiosk_state.open_app_ids
+        try:
+            idx = ids.index(kiosk_state.active_app_id)
+        except ValueError:
+            idx = 0
+        kiosk_state.active_app_id = ids[(idx + direction) % len(ids)]
+        self._broadcast_state()
+
+    def _broadcast_state(self):
+        emit('state', kiosk_state.to_dict(), broadcast=True, namespace='/general')
---
+++ backend/tests/test_general_socket.py
@@ -0,0 +1,68 @@
+import pytest
+from app import create_app
+from extensions import socketio
+from state import kiosk_state
+
+
+@pytest.fixture(autouse=True)
+def reset_state():
+    kiosk_state.active_app_id = None
+    kiosk_state.open_app_ids = []
+    yield
+    kiosk_state.active_app_id = None
+    kiosk_state.open_app_ids = []
+
+
+@pytest.fixture
+def socket_client(flask_app):
+    return socketio.test_client(flask_app, namespace='/general')
+
+
+def test_connect_receives_initial_state(socket_client):
+    received = socket_client.get_received('/general')
+    assert len(received) == 1
+    assert received[0]['name'] == 'state'
+    data = received[0]['args'][0]
+    assert data['active_app_id'] is None
+    assert data['open_app_ids'] == []
+
+
+def test_open_app_updates_state(socket_client):
+    socket_client.get_received('/general')  # clear connect event
+    socket_client.emit('open_app', {'app_id': 'hello-world'}, namespace='/general')
+    received = socket_client.get_received('/general')
+    assert any(e['name'] == 'state' for e in received)
+    state_event = next(e for e in received if e['name'] == 'state')
+    assert state_event['args'][0]['active_app_id'] == 'hello-world'
+    assert 'hello-world' in state_event['args'][0]['open_app_ids']
+
+
+def test_close_app_removes_from_state(socket_client):
+    socket_client.get_received('/general')
+    socket_client.emit('open_app', {'app_id': 'hello-world'}, namespace='/general')
+    socket_client.get_received('/general')
+    socket_client.emit('close_app', {'app_id': 'hello-world'}, namespace='/general')
+    received = socket_client.get_received('/general')
+    state_event = next(e for e in received if e['name'] == 'state')
+    assert 'hello-world' not in state_event['args'][0]['open_app_ids']
+    assert state_event['args'][0]['active_app_id'] is None
+
+
+def test_carousel_next_cycles_active_app(socket_client):
+    socket_client.get_received('/general')
+    socket_client.emit('open_app', {'app_id': 'hello-world'}, namespace='/general')
+    socket_client.get_received('/general')
+    socket_client.emit('carousel_next', namespace='/general')
+    received = socket_client.get_received('/general')
+    state_event = next(e for e in received if e['name'] == 'state')
+    assert state_event['args'][0]['active_app_id'] == 'hello-world'
+
+
+def test_carousel_prev_cycles_active_app(socket_client):
+    socket_client.get_received('/general')
+    socket_client.emit('open_app', {'app_id': 'hello-world'}, namespace='/general')
+    socket_client.get_received('/general')
+    socket_client.emit('carousel_prev', namespace='/general')
+    received = socket_client.get_received('/general')
+    state_event = next(e for e in received if e['name'] == 'state')
+    assert state_event['args'][0]['active_app_id'] == 'hello-world'

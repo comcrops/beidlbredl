862b92fea03159a1dc3d5ec1a08d9db8242ee15e
s-prechtl <stefan@tague.at>
2026-05-24 00:29:20 +0200

    feat: add Authentik SSO config and build vars

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,10 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', 'dev-secret')
     POCKETBASE_URL = os.environ.get('POCKETBASE_URL', 'http://localhost:8090')
+    AUTHENTIK_URL = os.environ.get('AUTHENTIK_URL', '')
+    AUTHENTIK_APP_SLUG = os.environ.get('AUTHENTIK_APP_SLUG', '')
+    AUTHENTIK_CLIENT_ID = os.environ.get('AUTHENTIK_CLIENT_ID', '')
+    KIOSK_SECRET = os.environ.get('KIOSK_SECRET', '')
     TESTING = False
 
 class TestConfig(Config):
